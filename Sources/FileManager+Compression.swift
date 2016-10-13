c2a704442f8ae62e89bc764be0e03ce1db7e2780
Ben Spratling IV <bspratling@mac.com>
2016-10-13 07:47:55 -0500

    Add Gzip support to FileManager

--- Sources/FileManager+Compression.swift
+++ Sources/FileManager+Compression.swift
@@ -18,44 +18,79 @@ extension FileManager {
 	///		the original file does not exist
 	///		the zlib has an error
 	///		the disk runs out of space
-	public func decompress(item zipFile:URL, with:CompressionTechnique, into directory:URL, progress:CompressionProgressHandler? = nil)throws {
+	public func decompress(item compressedFile:URL, using technique:CompressionTechnique, into directory:URL, progress:CompressionProgressHandler? = nil)throws->[URL] {
+		switch technique {
+		case .deflate:
+			return try unzip(item: compressedFile, into: directory, progress: progress)
+		case .gzip:
+			return try gunzip(item: compressedFile, into: directory, progress: progress)
+		}
+	}
+	
+	//  items must exist, item must be a file, which might be overwritten
+	public func compress(items:[URL], using technique:CompressionTechnique, to item:URL, progress:CompressionProgressHandler? = nil)throws {
+		switch technique {
+		case .deflate:
+			fatalError()
+		case .gzip:
+			try gzip(item: items, to: item, progress: progress)
+		}
+	}
+	
+	func unzip(item compressedFile:URL, into directory:URL, progress:CompressionProgressHandler? = nil)throws->[URL] {
 		//TODO: add progress handling
-		let zipData:Data = try Data(contentsOf: zipFile)
+		let zipData:Data = try Data(contentsOf: compressedFile)
 		let zipWrapper = try ZipDirectoryWrapping(zippedData: zipData)
-		try writeSubResources(in:zipWrapper, into:directory)
+		return try writeSubResources(in:zipWrapper, into:directory)
 	}
 	
 	//to be used recursively, creates directories as needed and overwrites
-	private func writeSubResources(in wrapper:SubResourceWrapping, into directory:URL)throws {
+	private func writeSubResources(in wrapper:SubResourceWrapping, into directory:URL)throws->[URL] {
 		//create the directory if needed
 		var isDirectory:ObjCBool = false
 		if !fileExists(atPath: directory.path, isDirectory: &isDirectory) || !isDirectory.boolValue {
 			try createDirectory(at: directory, withIntermediateDirectories: true, attributes: nil)
 		}
+		var writtenURLs:[URL] = []
 		for (_,resource) in wrapper.subResources {
 			//TODO: parallelize?
 			let subURL = directory.appendingPathComponent(resource.lastPathComponent)
 			if let dataWrapper = resource as? DataWrapping {
 				let data = dataWrapper.contents
 				try data.write(to: subURL, options:[])
+				writtenURLs.append(subURL)
 			} else if let dirWrapper = resource as? SubResourceWrapping {
-				try writeSubResources(in: dirWrapper, into: subURL)
+				writtenURLs.append(contentsOf: try writeSubResources(in: dirWrapper, into: subURL))
 			}
 		}
+		return writtenURLs
 	}
 	
-	/*
-	/// Given an array of regular file URL's, write them all into a .zip file using the deepest common ancestor as the implied directory into the given `file:URL`.  If it exits, it is overritten.
-	/// Throws errors when any original file does not exist, , if writing to the supplied URL fails, or if there is an internal zlib error
-	public func compress(items:[URL], with:CompressionTechnique, to file:URL, progress:CompressionProgressHandler? = nil)throws {
-		//TODO: write me
+	
+	func gunzip(item compressedFile:URL, into directory:URL, progress:CompressionProgressHandler? = nil)throws->[URL] {
+		let gzipData:Data = try Data(contentsOf: compressedFile)
+		let gzipper:GZipDataWrapping = try GZipDataWrapping(compressedData:gzipData)
+		var unzippedName:String = gzipper.lastPathComponent
+		if unzippedName.isEmpty {
+			if compressedFile.pathExtension == "gz" {
+				unzippedName = compressedFile.deletingLastPathComponent().lastPathComponent
+			}
+		}
+		let targetURL:URL = directory.appendingPathComponent(unzippedName)
+		//TODO: assuming slow write, create a randomly named temp file, write to it, and atomic replace?  is atomic replace a thing?
+		try gzipper.contents.write(to: targetURL, options: .atomic)
+		return [targetURL]
 	}
 	
-	/// zips a regular file, or a directory and all its contents into a zip file at the given URL
-	public func compress(item file:URL, with:CompressionTechnique, to zippedFile:URL, progress:CompressionProgressHandler? = nil)throws {
-		//TODO: write me
+	
+	func gzip(item files:[URL], to destination:URL, progress:CompressionProgressHandler? = nil)throws {
+		//In a gzip file, there can only be one
+		guard let oneFile:URL = files.first, files.count == 1 else { throw CompressionError.unsuppotedFormat }
+		let fileWrapper = FileWrapping(data: Data(), name: "")	//I should be able to init this from a URL
+		try fileWrapper.read(from: oneFile)
+		let gzipper:GZipDataWrapping = try GZipDataWrapping(fileWrapper)
+		try gzipper.serializedRepresentation.write(to: destination, options: [.atomicWrite])
 	}
 	
-*/
 }
 
